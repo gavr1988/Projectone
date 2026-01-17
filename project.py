17337e857fb0b25c2f4837998ac6a6c31ddfa327
gavr1988 <gavrewt@gmail.com>
2026-01-17 18:41:01 +0000

    about to replace the NaN values

--- project.py
+++ project.py
@@ -19,3 +19,35 @@ print (features_data.head())
 
 
 
+print ("information about data frame sales_data:")
+#getting a technical summary of the data frame
+print (sales_data.info())
+
+#Cleaning the data
+
+#Checking for NaN values in data frames
+
+print ("Checking for NaN values in sales_data:")
+
+if sales_data.isnull().any().any():
+    print ("Yes, there are NaN values in stores_data")
+else:
+    print ("No, NaN in stores_data")
+
+print ("Checking for NaN values in stores_data:")
+if stores_data.isnull().any().any():
+    print ("Yes, there are NaN values in stores_data")
+else:
+    print ("No NaN in stores_data")
+
+print ("checking for NaN values in features_data:")
+if features_data.isnull().any().any():
+    print ("Yes, there are NaN values in features_data")
+else:
+    print ("No NaN in features_data")
+
+#Now dealing with NaN values in data frames
+
+
+print (features_data.isnull().sum())
+
