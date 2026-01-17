beeac2e7a9fbf02fc2e12b73cb5612417a12f6f5
gavr1988 <gavrewt@gmail.com>
2026-01-17 19:16:59 +0000

    after changing the date to datetime object

--- project.py
+++ project.py
@@ -48,6 +48,51 @@ else:
 
 #Now dealing with NaN values in data frames
 
-
 print (features_data.isnull().sum())
 
+print ("Removing the NaN values from features_data by calculating the mean:")
+
+avg_markdown1= features_data['MarkDown1'].mean()
+features_data['MarkDown1'].fillna(avg_markdown1, inplace=True)
+
+avg_markdown2= features_data ['MarkDown2'].mean()
+features_data['MarkDown2'].fillna(avg_markdown2, inplace=True)
+
+avg_markdown3 = features_data ['MarkDown3'].mean()
+features_data['MarkDown3'].fillna(avg_markdown3, inplace=True)
+
+avg_markdown4= features_data ['MarkDown2'].mean()
+features_data['MarkDown4'].fillna(avg_markdown4, inplace=True)
+
+avg_markdown5= features_data ['MarkDown5'].mean()
+features_data['MarkDown5'].fillna(avg_markdown5, inplace=True)
+
+avg_CPI= features_data ['CPI'].mean()
+features_data['CPI'].fillna(avg_CPI, inplace=True)
+
+avg_unemployment= features_data ['Unemployment'].mean()
+features_data['Unemployment'].fillna(avg_unemployment, inplace=True)
+
+#Checking again for NaN values in all CSV files
+
+
+#Checking Number of NaN values
+print ("Number of NaN values in sales_data after cleaning")
+print (sales_data.isnull().sum())
+print ("Number of NaN values in stores_data after cleaning")
+print (stores_data.isnull().sum())
+print ("Number of NaN values in features_data after cleaning")
+print (features_data.isnull().sum())
+
+#Transforming
+#There are no date values for conversion in stores data frame
+#phase one converting date to datetime object
+#there was an issue with this so after some AI help I have found I need to add the dayfirst = true as is standard EU/UK Notation
+
+print("Converting date column in sales_data to datetime object")
+sales_data['Date'] = pd.to_datetime(sales_data['Date'], dayfirst=True)
+print(sales_data.info())
+
+print("Converting date column in features_data to datetime object")
+features_data['Date'] = pd.to_datetime(features_data['Date'], dayfirst=True)
+print(features_data.info())
